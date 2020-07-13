31971cb40f21a1fca6734e184516b9ac833788fd
Deepanshu <deepanshuchg@gmail.com>
2020-07-13 23:48:21 +0530

    Added more functionalities.

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 # display setup
 pygame.init()
@@ -11,6 +11,7 @@ pygame.display.set_caption("Zero21")
 
 # fonts
 NUMBER_FONT = pygame.font.SysFont('comicsans', 40)
+WORD_FONT = pygame.font.SysFont('comicsans', 40)
 
 # load images
 RED_CARD_IMAGE = pygame.image.load('Assets/RedCard.png')
@@ -19,7 +20,7 @@ GREEN_CARD_IMAGE = pygame.image.load('Assets/GreenCard.png')
 bg = pygame.image.load('Assets/background.jpeg')
 
 # game variables
-score = -10
+score = +10
 
 # colors
 WHITE = (205,219,245)
@@ -32,17 +33,19 @@ run = True
 
 
 class RedCard(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, num):
         self.x = x
         self.y = y
+        self.num = num
     
     def draw(self, win):
         win.blit(RED_CARD_IMAGE, (self.x, self.y))
 
 class BlueCard(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, num):
         self.x = x
         self.y = y
+        self.num = num
     
     def draw(self, win):
         win.blit(BLUE_CARD_IMAGE, (self.x, self.y))
@@ -58,16 +61,32 @@ def redraw():
     for card in blue_cards:
         card.draw(win)
     
+    
+    red_num = NUMBER_FONT.render(str(red_cards[-1].num), 1, WHITE)
+    win.blit(red_num, (red_cards[-1].x + 12, red_cards[-1].y + 12))
+    
+    blue_num = NUMBER_FONT.render(str(blue_cards[-1].num), 1, WHITE)
+    win.blit(blue_num, (blue_cards[-1].x + 12, blue_cards[-1].y + 12))
+
     win.blit(GREEN_CARD_IMAGE, (GAME_WIDTH/2, 500))
     text = NUMBER_FONT.render(str(score), 1, WHITE)
     win.blit(text, (GAME_WIDTH/2 + 5, 520))
     
     pygame.display.update()
 
+def display_message_lost():
+    pygame.time.delay(2000)
+    win.blit(bg, (0,0))
+    text = WORD_FONT.render("Sorry, you lost!", 1, BLACK)
+    win.blit(text, (30, 300))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
+
 red_cards = []
 r_x, r_y = 100,300
 for i in range(7):
-    card = RedCard(r_x, r_y)
+    card = RedCard(r_x, r_y, random.randint(-10,10))
     red_cards.append(card)
     r_x += 10
     r_y += 10
@@ -75,14 +94,14 @@ for i in range(7):
 blue_cards = []
 b_x, b_y = 300,300
 for i in range(7):
-    card = BlueCard(b_x, b_y)
+    card = BlueCard(b_x, b_y, random.randint(-10,10))
     blue_cards.append(card)
     b_x -= 10
     b_y += 10
 
 while run:
     clock.tick(FPS)
-    redraw()
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -90,7 +109,20 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             m_x, m_y = pygame.mouse.get_pos()
             print(f"{m_x} , {m_y}")
-            for red_card in red_cards:
-                if m_x > red_card.x and m_x < red_card.x + 40 and m_y > red_card.y and m_y < red_card.y + 60:
-                    print("FOUND")
+            if m_x > red_cards[-1].x and m_x < red_cards[-1].x + 40 and m_y > red_cards[-1].y and m_y < red_cards[-1].y + 60:
+                print("FOUND")
+                score += red_cards[-1].num
+                red_cards.pop()
+            if m_x > blue_cards[-1].x and m_x < blue_cards[-1].x + 40 and m_y > blue_cards[-1].y and m_y < blue_cards[-1].y + 60:
+                print("FOUND")
+                score += blue_cards[-1].num
+                blue_cards.pop()
+
+    redraw()
+    
+    if score > 21 or score < 0:
+        print("inside if")
+        display_message_lost()
+        break
+                
 
